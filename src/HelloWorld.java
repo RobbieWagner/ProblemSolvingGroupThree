dafc9e2444cb1815e0ca9f8d56bedf5e4f375cdb
tristan-bocook <tristan.bocook@my.simpson.edu>
2022-11-01 11:54:04 -0500

    Self-loathing
    :alien:

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,2 +1,6 @@
 public class HelloWorld {
+    int[] surrogates = {0xD83D, 0xDC7D};
+    String alienEmojiString = new String(surrogates, 0, surrogates.length);
+System.out.println(alienEmojiString);
+System.out.println("\uD83D\uDC7D");   // alternative way
 }
