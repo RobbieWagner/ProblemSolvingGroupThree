9c49501a1c3f01860f307a3a4293289948a2ab0e
Robbie Wagner <robbie.wagner@my.simpson.edu>
2022-11-03 12:19:50 -0500

    declare new functions

--- src/TowerOfHanoi.java
+++ src/TowerOfHanoi.java
@@ -1,40 +1,52 @@
-import java.util.Scanner;
+import java.util.*;
 
 public class TowerOfHanoi {
     public static void main(String[] args) {
         final Scanner scanner = new Scanner(System.in);
 
-        final int input = scanner.nextInt();
+        final int rings = scanner.nextInt();
         scanner.close();
 
         // this is important to know where to start
-        if (input % 2 == 0) {
+        if (rings % 2 == 0) {
             final boolean isEven = true;
         }
         else {final boolean isEven = false;}
 
-        final int moves = calculateMoves(input);
+        final int moves = calculateMoves(rings);
         System.out.println(moves);
+
+        Stack stack1 = new Stack<Integer>(), stack2 = new Stack<Integer>(), stack3 = new Stack<Integer>();
+
+        for(int i = rings; i > 0; i--) stack1.push(i);
 
         int currentMove = 1;
         while(currentMove <= moves)
         {
-            printNextHanoiMove(currentMove);
+            printNextHanoiMove(currentMove, rings);
             currentMove++;
         }
     }
 
-    private static int calculateMoves(int input) {
-        return (int) Math.round(Math.pow(2, input) - 1);
+    private static int calculateMoves(int rings) {
+        return (int) Math.round(Math.pow(2, rings) - 1);
     }
 
-    private static void printNextHanoiMove(int currentMove) {
-        //print the next hanoi move.
-        //print should be the stack you are pulling the top ring from, and then the one you place down.
-        //Ex. input 2, moves are:
-            //move top ring from column 1 to column 2: 1 2
-            //move top ring from column 1 to column 3: 1 3
-            //move top ring from column 2 to column 3: 1 3
-        //odd rings go 1 -> 2 -> 3, even rings go 1 -> 3 -> 2
+    private static void printNextHanoiMove(int currentMove, int rings) {
+        if(currentMove % 2 == 1) {
+            if(rings % 2 == 0) moveFirstRing(true);
+            else moveFirstRing(false);
+        }
+        else {
+            makeValidMove();
+        }
+    }
+
+    private static void moveFirstRing(boolean moveRight) {
+
+    }
+
+    private static void makeValidMove() {
+
     }
 }
