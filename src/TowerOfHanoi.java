72934b4219068a22306966d0c7402a915aaa68eb
Robbie Wagner <robbie.wagner@my.simpson.edu>
2022-11-03 12:52:33 -0500

    Update TowerOfHanoi.java

--- src/TowerOfHanoi.java
+++ src/TowerOfHanoi.java
@@ -8,22 +8,20 @@ public class TowerOfHanoi {
         scanner.close();
 
         // this is important to know where to start
-        if (rings % 2 == 0) {
-            final boolean isEven = true;
-        }
-        else {final boolean isEven = false;}
+        boolean right = false;
+        if (rings % 2 == 0) {right = true;}
 
         final int moves = calculateMoves(rings);
         System.out.println(moves);
 
-        Stack stack1 = new Stack<Integer>(), stack2 = new Stack<Integer>(), stack3 = new Stack<Integer>();
+        Stack[] stacks = {new Stack<Integer>(), new Stack<Integer>(), new Stack<Integer>()};
 
-        for(int i = rings; i > 0; i--) stack1.push(i);
+        for(int i = rings; i > 0; i--) stacks[0].push(i);
 
         int currentMove = 1;
         while(currentMove <= moves)
         {
-            printNextHanoiMove(currentMove, rings);
+            printNextHanoiMove(currentMove, rings, right, stacks);
             currentMove++;
         }
     }
@@ -32,21 +30,66 @@ public class TowerOfHanoi {
         return (int) Math.round(Math.pow(2, rings) - 1);
     }
 
-    private static void printNextHanoiMove(int currentMove, int rings) {
+    private static void printNextHanoiMove(int currentMove, int rings, boolean right, Stack<Integer>[] stacks) {
         if(currentMove % 2 == 1) {
-            if(rings % 2 == 0) moveFirstRing(true);
-            else moveFirstRing(false);
+            moveFirstRing(right, stacks);
         }
         else {
-            makeValidMove();
+            makeValidMove(stacks);
         }
     }
 
-    private static void moveFirstRing(boolean moveRight) {
+    private static void moveFirstRing(boolean moveRight, Stack<Integer>[] stacks) {
+        int stackFrom;
+        int stackTo;
 
+        for(int i = 0; i < stacks.length; i++) {
+            if(stacks[i].peek() == 1) {
+                int ringToMove =stacks[i].pop();
+
+                if(moveRight && i != 2) {
+                    stackFrom = i;
+                    stackTo = i+1;
+                    stacks[i + 1].push(ringToMove);
+                }
+                else if(moveRight) {
+                    stacks[0].push((ringToMove));
+                    stackFrom = i;
+                    stackTo = 0;
+                }
+                else if(!moveRight && i != 0){
+                    stackFrom = i;
+                    stackTo = i-1;
+                    stacks[-1].push(ringToMove);
+                }
+                else {
+                    stackFrom = i;
+                    stackTo = 2;
+                    stacks[2].push(ringToMove);
+                }
+                System.out.println((stackFrom + 1) + " " + (stackTo + 1));
+                return;
+            }
+        }
     }
 
-    private static void makeValidMove() {
+    private static void makeValidMove(Stack<Integer>[] stacks) {
+        int stackFrom = 3;
+        int stackTo = 3;
+        int smallestRing = 17;
 
+        for(int i = 0; i < stacks.length; i++) {
+            int currentRing = stacks[i].peek();
+            if(currentRing != 1 && currentRing < smallestRing) {
+                smallestRing = currentRing;
+                stackFrom = i;
+            }
+            else if(stacks[i].peek() != 1) stackTo = i;
+        }
+
+        int ring = stacks[stackFrom].pop();
+        stacks[stackTo].push(ring);
+
+        System.out.println((stackFrom + 1) + " " + (stackTo + 1));
     }
 }
