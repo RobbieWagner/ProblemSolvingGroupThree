46e5186d12e0292d737fb42b2b6bb7a3beeb5b1b
Tyler <erdmantyler@gmail.com>
2022-11-08 10:55:56 -0600

    Added Josephus

---
+++ src/Josephus_II.java
@@ -0,0 +1,16 @@
+public class Josephus_II {
+    /*Consider a game where there are n children (numbered 1,2,…,n) in a circle. During the game,
+     repeatedly k children are skipped and one child is removed from the circle.
+      In which order will the children be removed?
+
+Input
+The only input line has two integers n and k.
+
+Output
+Print n integers: the removal order.
+Example
+Input:
+7 2
+Output:
+3 6 2 7 5 1 4*/
+}
