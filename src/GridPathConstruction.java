7315235baa584878b737889cf1fc9e0b82d03f72
Robbie Wagner <robbie.wagner@my.simpson.edu>
2022-11-17 12:51:11 -0600

    Moved into same file (for submission)

--- src/GridPathConstruction.java
+++ src/GridPathConstruction.java
@@ -26,3 +26,46 @@ public class GridPathConstruction {
         }
     }
 }
+
+class Grid {
+
+    public int m;
+    public int n;
+
+    public int startX;
+    public int startY;
+
+    public int endX;
+    public int endY;
+
+    public Grid(int mIn, int nIn, int startXIn, int startYIn, int endXIn, int endYIn) {
+        m = mIn;
+        n = nIn;
+        startX = startXIn;
+        startY = startYIn;
+        endX = endXIn;
+        endY = endYIn;
+    }
+
+    public String toString(){
+        return "m: " + m + "\nn: " + n + "\nStartPos: " + startX + " " + startY + "\nEndPos: " + endX + " " + endY;
+    }
+
+    public boolean canBeTraversed(){
+        return true;
+    }
+}
+
+class Node {
+    public int connections;
+    public boolean traversed;
+
+    public Node(int connectionsIn) {
+        connections = connectionsIn;
+        traversed = false;
+    }
+
+    public String toString(){
+        return "connections: " + connections + "\ntraversed? " + traversed;
+    }
+}
