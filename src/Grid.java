cb68ab2bbc4ad9a92988731b7b39f46277029b58
Robbie Wagner <robbie.wagner@my.simpson.edu>
2022-11-17 12:26:42 -0600

    Grid Class

---
+++ src/Grid.java
@@ -0,0 +1,24 @@
+public class Grid {
+
+    public int m;
+    public int n;
+
+    public int startX;
+    public int startY;
+
+    public int endX;
+    public int endY;
+
+    public Grid(int mIn, int nIn, int startXIn, int startYIn, int endXIn, int endYIn) {
+        m = mIn;
+        n = nIn;
+        startX = startXIn;
+        startY = startYIn;
+        endX = endXIn;
+        endY = endYIn;
+    }
+
+    public String toString(){
+        return "m: " + m + "\nn: " + n + "\nStartPos: " + startX + " " + startY + "\nEndPos: " + endX + " " + endY;
+    }
+}
--- src/GridPathConstruction.java
+++ src/GridPathConstruction.java
@@ -1,5 +1,24 @@
+import java.util.*;
+
 public class GridPathConstruction {
     public static void main(String[] args) {
-        
+        Scanner scan = new Scanner(System.in);
+
+        int testCases = scan.nextInt();
+        ArrayList<Grid> Grids= new ArrayList<Grid>();
+
+        for(int i = 0; i < testCases; i++){
+            int m = scan.nextInt();
+            int n = scan.nextInt();
+            int startX = scan.nextInt();
+            int startY = scan.nextInt();
+            int endX = scan.nextInt();
+            int endY = scan.nextInt();
+
+            Grid grid = new Grid(m, n, startX, startY, endX, endY);
+            Grids.add(grid);
+
+            System.out.println(grid.toString());
+        }
     }
 }
